3f375303d21b632164254856c5e99cbe8bb90d26
abhijit19 <abhijit@sidedooraccess.com>
2021-11-01 23:54:29 -0700

    Changes for artist registration and typescript changes

---
+++ e2e/cypress/integration/artistRegistration.ts
@@ -0,0 +1,57 @@
+import url from '../fixtures/urls.json'
+import btnText from '../fixtures/btn_text.json'
+import loginCreds from '../fixtures/login_credentials.json'
+import input from '../fixtures/input_options.json'
+
+describe('Creating a new artist user', () => {
+    
+    var randomEmailType = require('random-email')({ domain: 'gmail.com' });
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false
+    })
+    it('Navigate to Side Door home page', () => {
+        cy.visit((url.base_url))
+        cy.url().should('include', url.home_url)
+    })
+
+    it('Click on Join button, select perform option and navigate to signup page', () => {
+        cy.contains(btnText.home_join_btn).trigger('mouseover')
+        cy.get(btnText.home_join_perform_btn).click()
+        cy.url().should('include', url.artist_signup_url)
+    })
+
+    it('Verify social signup buttons and email signup buttons are visible', () => {
+        cy.get(btnText.artist_signup_facebook).should('be.visible')
+        cy.get(btnText.artist_signup_google).should('be.visible')
+        cy.get(btnText.artist_signup_twitter).should('be.visible')
+        cy.get(btnText.artist_signup_email).should('be.visible')
+    })
+
+    it('Create account with email by inputting required details', () => {
+        cy.get(btnText.artist_signup_email).click()
+        cy.get('#email').type(randomEmailType)
+        cy.get('#confirmEmail').type(randomEmailType)
+        cy.get('#firstName').type(loginCreds.artistFirstName)
+        cy.get('#lastName').type(loginCreds.artistLastName)
+        cy.get('#password').type(loginCreds.password)
+        cy.get('#confirmPassword').type(loginCreds.password)
+        cy.get('input[name="acceptsTos"]').check()
+        cy.get(btnText.save_and_continue).click()     
+    })
+
+    it('Input artist specific details and save the profile', () => {
+        cy.url().should('include', url.artist_signup_profile)
+        cy.get('#name').type(input.artistName)
+        cy.get('#actType').click()
+        cy.contains(input.performanceType).should('be.visible').click()
+        cy.get('#compiledAddress').type(input.artistLocation)
+        cy.get('#compiledAddress-option-0').should('be.visible').click()
+        cy.fixture('testArtistImage.jpg').then(fileContent => {
+            cy.get('input[type="file"]').attachFile({
+                fileContent: fileContent.toString(),
+                fileName: 'testArtistImage.jpg'
+            });
+        });
+    });
+
+})
--- e2e/cypress/integration/audienceRegistration.ts
+++ e2e/cypress/integration/audienceRegistration.ts
@@ -2,47 +2,47 @@ import url from '../fixtures/urls.json'
 import btnText from '../fixtures/btn_text.json'
 import loginCreds from '../fixtures/login_credentials.json'
 
-describe('Creating a new User', () => {
+describe('Creating a new audience user ', () => {
     
     var randomEmailType = require('random-email')({ domain: 'gmail.com' });
     Cypress.on('uncaught:exception', (err, runnable) => {
         return false
     })
-    it('Navigate to Side Door home page', function(){
+    it('Navigate to Side Door home page', () => {
         cy.visit((url.base_url))
         cy.url().should('include', url.home_url)
     })
 
-    it('Click on Join button, select watch option and navigate to signup page', function(){
+    it('Click on Join button, select watch option and navigate to signup page', () => {
         cy.contains(btnText.home_join_btn).trigger('mouseover')
-        cy.contains(btnText.home_join_watch_btn).click()
+        cy.get(btnText.home_join_watch_btn).click()
         cy.url().should('include', url.watch_signup_url)
     })
 
-    it('Verify social signup buttons and email signup buttons are visible', function(){
-        cy.contains(btnText.watch_signup_facebook)
-        cy.contains(btnText.watch_signup_google)
-        cy.contains(btnText.watch_signup_twitter)
-        cy.contains(btnText.watch_signup_email)
+    it('Verify social signup buttons and email signup buttons are visible', () => {
+        cy.get(btnText.watch_signup_facebook).should('be.visible')
+        cy.get(btnText.watch_signup_google).should('be.visible')
+        cy.get(btnText.watch_signup_twitter).should('be.visible')
+        cy.get(btnText.watch_signup_email).should('be.visible')
     })
 
-    it('Create account with email by inputting required details', function(){
-        cy.contains(btnText.watch_signup_email).click()
+    it('Create account with email by inputting required details', () => {
+        cy.get(btnText.watch_signup_email).click()
         cy.get('#email').type(randomEmailType)
         cy.get('#confirmEmail').type(randomEmailType)
-        cy.get('#firstName').type(loginCreds.firstName)
-        cy.get('#lastName').type(loginCreds.lastName)
+        cy.get('#firstName').type(loginCreds.audienceFirstName)
+        cy.get('#lastName').type(loginCreds.audienceLastName)
         cy.get('#password').type(loginCreds.password)
         cy.get('#confirmPassword').type(loginCreds.password)
         cy.get('input[name="acceptsTos"]').check()
-        cy.contains(btnText.save_and_continue).click()     
+        cy.get(btnText.save_and_continue).click()     
     })
 
-    it('Verify successful account creation and log out', function(){
+    it('Verify successful account creation and log out', () => {
         cy.url().should('include', url.watch_profile_home)
         cy.get('#sda-nav-menu-open-button').click()
-        cy.contains(btnText.log_out).click()
-        cy.url().should('include', url.watch_profile_home)  
+        cy.get(btnText.log_out).click()
+        cy.get(btnText.log_in).should('be.visible')
 
     })
 
